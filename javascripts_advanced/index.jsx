36c9a10d1b8880e7a342576e9ec17e83bda69f4e
GalletaDev <macontor512@gmail.com>
2025-07-02 12:03:34 +0200

    ejemplo de html con tablas y estilos css avanzados

---
+++ javascripts_advanced/index.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import rectangle1 from "./rectangle-1.png";
+
+export const Box = () => {
+  return (
+    <div className="w-[2458px] h-[3136px]">
+      <img
+        className="fixed w-[2466px] h-[3144px] top-0 left-0"
+        alt="Rectangle"
+        src={rectangle1}
+      />
+    </div>
+  );
+};
